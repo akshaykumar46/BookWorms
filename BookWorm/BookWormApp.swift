d8fde79a0327cccf11b918e52518322151b3eb29
Akshay Kumar <akshay08022002@gmail.com>
2023-05-26 20:09:14 +0530

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by AKSHAY KUMAR on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
