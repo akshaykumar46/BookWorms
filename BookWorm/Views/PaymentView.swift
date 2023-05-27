79efccf22390519b823320abd1c19a2ca8359bf8
Akshay Kumar <akshay08022002@gmail.com>
2023-05-28 02:17:38 +0530

    healthy

---
+++ BookWorm/Views/PaymentView.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentView.swift
+//  BookWorm
+//
+//  Created by AKSHAY KUMAR on 28/05/23.
+//
+
+import SwiftUI
+//import Razorpay
+struct PaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaymentView_Previews: PreviewProvider {
+    static var previews: some View {
+        PaymentView()
+    }
+}
